2d4465c82f0d55756cb42beb61857594bff38606
Nicholas Matsakis <niko@alum.mit.edu>
2009-12-01 10:50:45 +0100

    rewrite BBPC in a cleaner style

--- Intervals/test/ch/ethz/intervals/TestBBPC.java
+++ Intervals/test/ch/ethz/intervals/TestBBPC.java
@@ -1,117 +1,115 @@
 package ch.ethz.intervals;
 
+import static ch.ethz.intervals.Intervals.addHb;
 import static ch.ethz.intervals.Intervals.blockingInterval;
-import static ch.ethz.intervals.Intervals.end;
-import static ch.ethz.intervals.Intervals.intervalDuring;
-import static ch.ethz.intervals.Intervals.intervalWithBound;
-import static ch.ethz.intervals.Intervals.start;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.junit.Assert;
 import org.junit.Test;
 
-import ch.ethz.intervals.quals.ObjectParameter;
-
-@ObjectParameter
-@interface BBPC_P {
-    String value() default "BBPC_P";
-}
-
-@ObjectParameter
-@interface BBPC_C {
-    String value() default "BBPC_C";
-}
-
 /**
- * Bounded-Buffer Producer Consumer Example.
- *
+ * Bounded-Buffer Producer Consumer example
+ * using arrays.
  */
 public class TestBBPC {
-	
+
 	public final int N = 3;
 	public final int M = 100;
-	
+
 	static AtomicInteger stamp = new AtomicInteger(1);
+
 	public int stamp() {
 		return stamp.getAndIncrement();
 	}
-	
+
 	int[] consumed = new int[M];
 	int[] produced = new int[M];
 
-	class Producer extends AbstractTask {
-		final int i;
-		final Interval consumers[];
-		final Interval prevConsumer;
-		
-		Producer(int i, Interval[] consumers,	Interval prevConsumer) {
-			this.i = i;
-			this.consumers = consumers;
-			this.prevConsumer = prevConsumer;
-		}
-		
-		public Integer produceData() { return i; }
+	public class BBPC extends AbstractTask {
 
-		public void run(Point currentEnd) {
-			if(i >= M) 
-				return;
-			produced[i] = stamp();
-			
-			Integer data = produceData();
-			Consumer consTask = new Consumer(data);
-			Interval consInter = intervalWithBound(currentEnd.bound(), consTask);
-			Intervals.addHb(end(prevConsumer), consInter.start());
-			consumers[i % N] = consInter;
-			
-			int nxtI = (i+1) % N;
-			Interval waitInter = consumers[nxtI]; 
+		final int N = 3;
+		final Interval[] producers = new Interval[N];
+		final Interval[] consumers = new Interval[N];
+
+		class Producer extends AbstractTask {
+			final int index;
+
+			public Producer(int index) {
+				this.index = index;
+			}
+
+			public String toString() {
+				return "Producer["+index+"]";
+			}
+
+			public void addDependencies(Interval inter) {
+				// comes after the previous producer:
+				if (index > 0)
+					addHb(producers[(index - 1) % N].end(), inter.start());
 				
-			Producer nxtTask = new Producer(i + 1, consumers, consInter);
-			Interval nxtInter = intervalWithBound(currentEnd.bound(), nxtTask);
-			Intervals.addHb(end(waitInter), nxtInter.start());
+				// and the consumer N indices before:
+				if (index >= N)
+					addHb(consumers[(index - N) % N].end(), inter.start());
+			}
+
+			public void run(Point end) {
+				produced[index] = (index * 2);
+				if(index + 1 < M)
+					producers[(index + 1) % N] = 
+						Intervals.siblingInterval(new Producer(index + 1));
+				else
+					producers[(index + 1) % N] = null;
+			}
 		}
-		
-		public String toString() {
-			return String.format("Producer[%d]", i);
+
+		class Consumer extends AbstractTask {
+			final int index;
+			
+			public Consumer(int index) {
+				this.index = index;
+			}
+			
+			public String toString() {
+				return "Consumer["+index+"]";
+			}
+
+			public void addDependencies(Interval inter) {
+				// comes after the producer:
+				addHb(producers[index % N].end(), inter.start());
+				
+				// and after the previous consumer:
+				if (index > 0)
+					addHb(consumers[(index - 1) % N].end(), inter.start());
+			}
+
+			public void run(Point end) {
+				consumed[index] = produced[index];
+				if(producers[(index + 1) % N] != null)
+					consumers[(index + 1) % N] = 
+						Intervals.siblingInterval(new Consumer(index + 1));
+			}
+		}
+
+		public void run(Point end) {
+			producers[0] = Intervals.childInterval(new Producer(0));
+			consumers[0] = Intervals.childInterval(new Consumer(0));
 		}
 
 	}
-	
-	class Consumer extends AbstractTask {
-		public Integer i;
-		
-		private Consumer(Integer i) {
-			this.i = i;
-		}
-		
-		public String toString() {
-			return String.format("Consumer[%d]", i);
-		}
 
-		public void run(Point currentEnd) {
-			consumed[i] = stamp();
-		}
-	}
-	
-	@Test public void test() {
-//		ExecutionLog.enableGui();
-		blockingInterval(new AbstractTask() {
-			public void run(Point parentEnd) {
-				Producer fstProd = new Producer(0, new Interval[N], null);
-				intervalWithBound(parentEnd, fstProd);
-			}			
-		});
-//		ExecutionLog.disable();
-		
-		for(int i = 1; i < M; i++)
-			Assert.assertTrue(
-					"i="+i+" consumed[i-1]="+consumed[i-1]+" consumed[i]="+consumed[i],
-					consumed[i-1] < consumed[i]);
-		for(int i = N; i < M; i++)
-			Assert.assertTrue(
-					"i="+i+" produced[i]="+produced[i]+" consumed[i-N]="+consumed[i-N],
-					produced[i] > consumed[i-N]); 
+	@Test
+	public void test() {
+		blockingInterval(new BBPC());
+
+		for (int i = 1; i < M; i++)
+			Assert.assertTrue("i=" + i + " consumed[i-1]=" + consumed[i - 1]
+					+ " consumed[i]=" + consumed[i],
+					consumed[i - 1] < consumed[i]);
+		for (int i = N; i < M; i++)
+			Assert.assertTrue("i=" + i + " produced[i]=" + produced[i]
+					+ " consumed[i-N]=" + consumed[i - N],
+					produced[i] > consumed[i - N]);
 	}
 
 }
