0391f090f936f8532220f8144731d18c56fffe1f
Nicholas Matsakis <nmatsaki@mafushi.inf.ethz.ch>
2009-12-02 22:47:50 +0100

    update benchmark script to work with older gits, new measurements

--- Intervals/benchmark-init.sh
+++ Intervals/benchmark-init.sh
@@ -1,8 +1,8 @@
 echo Logfile: $1
 echo "> date"
 date
-echo "git show --online | head -1"
-git show --oneline | head -1
+echo 'git show --pretty=oneline | head -1'
+git show --pretty=oneline | head -1
 echo "git diff | cat"
 git diff | cat
 echo "pwd"
