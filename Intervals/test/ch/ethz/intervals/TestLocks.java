40ec3dd0a82c70d60f4b66d7f3a2312ce30aed64
Nicholas Matsakis <niko@alum.mit.edu>
2010-01-15 09:08:23 +0100

    add a comparable timing test for non-subintervals, since the subinter one caught somany bugs

--- Intervals/test/ch/ethz/intervals/TestLocks.java
+++ Intervals/test/ch/ethz/intervals/TestLocks.java
@@ -38,7 +38,8 @@ public class TestLocks {
 	}
 	
 	public static final int REPEAT = 300;
-	
+	public static final int REPEAT_SMALL = 25; // some tests sleep or otherwise take a long time
+
 	// Just test that locks work at all:
 	@Test public void simple() {
 		for(int i = 0; i < REPEAT; i++) {
@@ -90,37 +91,43 @@ public class TestLocks {
 	}
 	
 	
-	// Test subinterval locking:
+	class LockingVoidSubinterval extends VoidSubinterval {
+		final String name;
+		final long[] times;
+		final List<String> ids;
+		final Lock l1;
+		
+		public LockingVoidSubinterval(String n, long[] times, List<String> ids, Lock l1) {
+			this.name = n;
+			this.times = times;
+			this.ids = ids;
+			this.l1 = l1;
+		}
+		@Override public String toString() {
+			return name;
+		}
+		@Override public Lock[] locks() {
+			return new Lock[] { l1 };
+		}
+		@Override public void run(Interval subinterval) {
+			times[0] = System.currentTimeMillis();
+			ids.add(name+":"+l1.isWritable());
+			try { // just to make it more likely that the lock fails
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+			}
+			times[1] = System.currentTimeMillis();
+		}
+	}
+	
+	/** Tests that two subintervals (within different master intervals) contending for a lock
+	 *  both eventually get it, but run in disjoint times: */
 	@Test public void subinterLock() {
-		for(int i = 0; i < 50; i++) {
+		for(int i = 0; i < REPEAT_SMALL; i++) {
 			final Lock l1 = new Lock();
 			final List<String> ids = Collections.synchronizedList(new ArrayList<String>());
 			final long[] aTimes = new long[2];
 			final long[] bTimes = new long[2];
-			
-			class LockingVoidSubinterval extends VoidSubinterval {
-				final String name;
-				final long[] times;
-				public LockingVoidSubinterval(String n, long[] times) {
-					this.name = n;
-					this.times = times;
-				}
-				@Override public String toString() {
-					return name;
-				}
-				@Override public Lock[] locks() {
-					return new Lock[] { l1 };
-				}
-				@Override public void run(Interval subinterval) {
-					times[0] = System.currentTimeMillis();
-					ids.add(name+":"+l1.isWritable());
-					try { // just to make it more likely that the lock fails
-						Thread.sleep(100);
-					} catch (InterruptedException e) {
-					}
-					times[1] = System.currentTimeMillis();
-				}
-			}
 			
 			Intervals.subinterval(new VoidSubinterval() {			
 				@Override public String toString() {
@@ -129,13 +136,13 @@ public class TestLocks {
 				@Override public void run(Interval subinterval) {
 					new Interval(subinterval, "inner1") {						
 						@Override protected void run() {
-							Intervals.subinterval(new LockingVoidSubinterval("a", aTimes));
+							Intervals.subinterval(new LockingVoidSubinterval("a", aTimes, ids, l1));
 						}
 					};
 					
 					new Interval(subinterval, "inner2") {						
 						@Override protected void run() {
-							Intervals.subinterval(new LockingVoidSubinterval("b", bTimes));
+							Intervals.subinterval(new LockingVoidSubinterval("b", bTimes, ids, l1));
 						}
 					};
 				}
@@ -152,6 +159,48 @@ public class TestLocks {
 					String.format("aTimes=(%s-%s) bTimes=(%s-%s)", aTimes[0], aTimes[1], bTimes[0], bTimes[1]),
 					aTimes[1] <= bTimes[0] || bTimes[1] <= aTimes[0]);
 		}
-	}
+	}	
 	
+	/** Like {@link #subinterLock()} but the locks are acquired by async intervals: */
+	@Test public void interLock() {
+		for(int i = 0; i < REPEAT_SMALL; i++) {
+			final Lock l1 = new Lock();
+			final List<String> ids = Collections.synchronizedList(new ArrayList<String>());
+			final long[] aTimes = new long[2];
+			final long[] bTimes = new long[2];
+			
+			Intervals.subinterval(new VoidSubinterval() {			
+				@Override public String toString() {
+					return "outer";
+				}
+				@Override public void run(Interval subinterval) {
+					Interval a = new Interval(subinterval, "a") {						
+						@Override protected void run() {
+							new LockingVoidSubinterval("a", aTimes, ids, l1).run(this);
+						}
+					};
+					Intervals.addExclusiveLock(a, l1);
+					
+					Interval b = new Interval(subinterval, "b") {						
+						@Override protected void run() {
+							new LockingVoidSubinterval("b", bTimes, ids, l1).run(this);
+						}
+					};
+					Intervals.addExclusiveLock(b, l1);
+				}
+			});
+			
+			Assert.assertEquals(2, ids.size());
+			
+			Assert.assertTrue(ids.contains("a:true"));
+			Assert.assertTrue(ids.contains("b:true"));
+			
+			Assert.assertTrue(aTimes[0] <= aTimes[1]);
+			Assert.assertTrue(bTimes[0] <= bTimes[1]);
+			Assert.assertTrue(
+					String.format("aTimes=(%s-%s) bTimes=(%s-%s)", aTimes[0], aTimes[1], bTimes[0], bTimes[1]),
+					aTimes[1] <= bTimes[0] || bTimes[1] <= aTimes[0]);
+		}
+	}
+
 }
