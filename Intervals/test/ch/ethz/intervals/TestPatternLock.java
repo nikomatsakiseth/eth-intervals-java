cc9820aa6fe0363ed9bebdf7248c32f6b2f4dd47
Nicholas Matsakis <niko@alum.mit.edu>
2010-05-18 09:03:01 +0200

    port TestPatternLock

--- Intervals/test/ch/ethz/intervals/TestPatternLock.java
+++ Intervals/test/ch/ethz/intervals/TestPatternLock.java
@@ -5,18 +5,21 @@ import org.junit.Test;
 import ch.ethz.intervals.impl.IntervalImpl;
 import ch.ethz.intervals.impl.LockImpl;
 import ch.ethz.intervals.impl.PointImpl;
+import ch.ethz.intervals.mirror.Interval;
+import ch.ethz.intervals.task.AbstractTask;
+import ch.ethz.intervals.task.SetupTask;
 
 public class TestPatternLock {
 	
-	public static class DebugInterval extends EmptyInterval {
+	public static class DebugInterval extends AbstractTask {
 
-		public DebugInterval(@ParentForNew("Parent") Dependency dep, String name) {
-			super(dep, name);
+		public DebugInterval(String name) {
+			super(name);
 		}
 		
 		@Override
-		public void run() {
-			System.err.printf("Executing: %s (%s-%s)\n", this, start, end);
+		public void run(Interval current) {
+			System.err.printf("Executing: %s (%s-%s)\n", this, current.getStart(), current.getEnd());
 		}
 		
 	}
@@ -37,32 +40,31 @@ public class TestPatternLock {
 			
 		final LockImpl l = new LockImpl();
 		
-		Intervals.inline(new VoidInlineTask() {
-			public void run(IntervalImpl subinterval) {
-				new SetupInterval(subinterval) {
-					@Override protected void setup(PointImpl setupEnd, IntervalImpl worker) {
-						IntervalImpl a = new DebugInterval(worker, ("a"));
-						IntervalImpl b = new DebugInterval(worker, ("b"));
-						IntervalImpl c = new DebugInterval(worker, ("c"));
-						IntervalImpl b1 = new DebugInterval(b, ("b1"));
-						IntervalImpl b11 = new DebugInterval(b, ("b11"));
-						IntervalImpl b2 = new DebugInterval(b, ("b2"));
-						
-						Intervals.addHb(a.end, b.start);
-						Intervals.addHb(b.end, c.start);
-						Intervals.addHb(b1.end, b11.start);
-						
-						IntervalImpl l1 = (IntervalImpl) new DebugInterval(b, ("l1"));
-						IntervalImpl l2 = (IntervalImpl) new DebugInterval(b, ("l2"));
-						
-						Intervals.addExclusiveLock(l1, l);
-						Intervals.addExclusiveLock(l2, l);
-						
-						System.out.printf("a=%s b=%s c=%s b1=%s b11=%s b2=%s l1=%s l2=%s setup.end=%s, worker=%s\n", 
-								a, b, c, b1, b11, b2, l1, l2, setupEnd, worker);					
-					}					
-				};
+		Intervals.inline(new SetupTask("outer") {
+			private Interval debugInterval(Interval worker, String name) {
+				return worker.newAsyncChild(new DebugInterval(name));
 			}
+			public void setup(Interval setup, Interval worker) {
+				Interval a = debugInterval(worker, ("a"));
+				Interval b = debugInterval(worker, ("b"));
+				Interval c = debugInterval(worker, ("c"));
+				Interval b1 = debugInterval(b, ("b1"));
+				Interval b11 = debugInterval(b, ("b11"));
+				Interval b2 = debugInterval(b, ("b2"));
+				
+				Intervals.addHb(a, b);
+				Intervals.addHb(b, c);
+				Intervals.addHb(b1, b11);
+				
+				Interval l1 = debugInterval(b, ("l1"));
+				Interval l2 = debugInterval(b, ("l2"));
+				
+				l1.addLock(l);
+				l2.addLock(l);
+				
+				System.out.printf("a=%s b=%s c=%s b1=%s b11=%s b2=%s l1=%s l2=%s setup=%s, worker=%s\n", 
+						a, b, c, b1, b11, b2, l1, l2, setup, worker);					
+			}					
 		});				
 	}
 }
